a1800ce060484e87ebaebe0107f82f764f238393
unionny <jadu1028@naver.com>
2020-12-30 23:06:31 +0900

    feat: Make 404 Not Found page

--- main.js
+++ main.js
@@ -5,40 +5,43 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
 
-    fs.readFile('data/'+title, 'utf8', function(err, content){
-        if(_url == '/'){
-            title = 'Welcome';
-          }
-          if(_url == '/favicon.ico'){
-              response.writeHead(404);
-              response.end();
-              return;
-          }
-          response.writeHead(200);
-          
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
+    console.log(url.parse(_url, true));
+
+    if(pathname === '/')
+    {
+        fs.readFile('data/'+title, 'utf8', function(err, content){
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
             <title>WEB1 - ${title}</title>
             <meta charset="utf-8">
-          </head>
-          <body>
+            </head>
+            <body>
             <h1><a href="/">WEB</a></h1>
             <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
             </ol>
             <h2>${title}</h2>
             <p>${content}</p>
-          </body>
-          </html>   
-          `;
-          response.end(template);
-    });
+            </body>
+            </html>   
+            `;
+            response.writeHead(200);
+            response.end(template);
+        });
+    }
+    else {
+        // 200: OK, 404: Not OK
+        response.writeHead(404);
+        response.end('Not found');
+    }
+
     
 });
-app.listen(80);
+app.listen(3000);
