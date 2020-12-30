6a3aaf420e0f493b5e043f05b4bc814fb3d47f2e
unionny <jadu1028@naver.com>
2020-12-30 17:42:01 +0900

    feat: Add template(Study Template Literal)

---
+++ template.js
@@ -0,0 +1,10 @@
+var name = 'k8805';
+var letter = 'Dear '+name+'\n\
+\n\
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit rerum non aliquid sit minus quasi id minima a perferendis quae quam ut accusantium, beatae magnam hic maxime voluptates, facere optio.'+name;
+
+var letter = `Dear ${name}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit rerum non aliquid sit minus quasi id minima a perferendis quae quam ut accusantium, beatae magnam hic maxime voluptates, facere optio. ${name} ${1+3}`;
+
+console.log(letter);
