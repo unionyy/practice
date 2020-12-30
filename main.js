a9a26e715b58a5ba9579a78b20ac0f1b60e43a21
unionny <jadu1028@naver.com>
2020-12-31 00:00:23 +0900

    refactor: Use function to remove recursion

--- main.js
+++ main.js
@@ -2,6 +2,23 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title, list, body) {
+    return `
+    <!doctype html>
+    <html>
+        <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+        </head>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            ${body}
+        </body>
+    </html>   
+    `;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -13,49 +30,20 @@ var app = http.createServer(function(request,response){
 
         fs.readdir('./data', function(err, filelist) {
             var list = '<ol>';
-
-                for(var fe of filelist) {
-                    list = `${list}<li><a href="/?id=${fe}">${fe}</a></li>`;
-                }
-                list = list + '</ol>';
+            for(var fe of filelist) {
+                list = `${list}<li><a href="/?id=${fe}">${fe}</a></li>`;
+            }
+            list = list + '</ol>';
             if(queryData.id === undefined) {
                     var title = 'Welcome!';
                     var content = 'Hello, Node.js!';
-                    var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    <p>${content}</p>
-                    </body>
-                    </html>   
-                    `;
+                    var template = templateHTML(title, list, `<h2>${title}</h2><p>${content}</p>`);
                     response.writeHead(200);
                     response.end(template);
             } else {
                 fs.readFile('data/'+queryData.id, 'utf8', function(err, content){
                     var title = queryData.id;
-                    var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    <p>${content}</p>
-                    </body>
-                    </html>   
-                    `;
+                    var template = templateHTML(title, list, `<h2>${title}</h2><p>${content}</p>`);
                     response.writeHead(200);
                     response.end(template);
                 });
