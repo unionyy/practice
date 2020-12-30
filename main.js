b9ee12a9b610d88ef6e6c43b8535b5a9c83017f4
unionny <jadu1028@naver.com>
2020-12-30 23:43:47 +0900

    feat: Automatic Updated List

--- main.js
+++ main.js
@@ -10,64 +10,63 @@ var app = http.createServer(function(request,response){
     console.log(url.parse(_url, true));
 
     if(pathname === '/') {
-        if(queryData.id === undefined) {
-            var title = 'Welcome!';
-            var content = 'Hello, Node.js!';
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-            </head>
-            <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${content}</p>
-            </body>
-            </html>   
-            `;
-            response.writeHead(200);
-            response.end(template);
 
-        } else {
-            fs.readFile('data/'+queryData.id, 'utf8', function(err, content){
-                var title = queryData.id;
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-                </head>
-                <body>
-                <h1><a href="/">WEB</a></h1>
-                <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                </ol>
-                <h2>${title}</h2>
-                <p>${content}</p>
-                </body>
-                </html>   
-                `;
-                response.writeHead(200);
-                response.end(template);
-            });
-        }
-        
+        fs.readdir('./data', function(err, filelist) {
+            var list = '<ol>';
+
+                for(var fe of filelist) {
+                    list = `${list}<li><a href="/?id=${fe}">${fe}</a></li>`;
+                }
+                list = list + '</ol>';
+            if(queryData.id === undefined) {
+                    var title = 'Welcome!';
+                    var content = 'Hello, Node.js!';
+                    var template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                    </head>
+                    <body>
+                    <h1><a href="/">WEB</a></h1>
+                    ${list}
+                    <h2>${title}</h2>
+                    <p>${content}</p>
+                    </body>
+                    </html>   
+                    `;
+                    response.writeHead(200);
+                    response.end(template);
+            } else {
+                fs.readFile('data/'+queryData.id, 'utf8', function(err, content){
+                    var title = queryData.id;
+                    var template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                    </head>
+                    <body>
+                    <h1><a href="/">WEB</a></h1>
+                    ${list}
+                    <h2>${title}</h2>
+                    <p>${content}</p>
+                    </body>
+                    </html>   
+                    `;
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            }
+        });
+            
     } else {
         // 200: OK, 404: Not OK
         response.writeHead(404);
         response.end('Not found');
     }
-
-    
+      
 });
 app.listen(3000);
