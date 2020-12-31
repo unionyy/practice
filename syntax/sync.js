fbfc7dc2a923d57287cc1acb5df89114a11081ef
unionny <jadu1028@naver.com>
2020-12-31 11:48:52 +0900

    feat: Practice Sync/Async

---
+++ syntax/sync.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+// //readFileSync
+// console.log('A');
+// var result = fs.readFileSync('synctest.txt', 'utf8');
+// console.log(result);
+// console.log('C');
+
+
+console.log('A');
+fs.readFile('synctest.txt', 'utf8', function(err, result) {
+    console.log(result);
+})
+console.log('C');
