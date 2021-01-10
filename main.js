df0d2840e75ecf3b1f1e3a209695c32beff89d6e
unionyy <jadu1028@naver.com>
2021-01-11 01:49:15 +0900

    feat: Show author using MySQL JOIN

--- main.js
+++ main.js
@@ -39,7 +39,9 @@ var app = http.createServer(function(request,response){
           if(err) {
             throw err;
           }
-          db.query('SELECT * FROM topic WHERE id=?', [queryData.id], function(err2, topic) {
+          db.query('SELECT * FROM topic LEFT JOIN author ON author_id=author.id WHERE topic.id=?',
+          [queryData.id],
+          function(err2, topic) {
             if(err2) {
               throw err2;
             }
@@ -47,7 +49,10 @@ var app = http.createServer(function(request,response){
             var description = topic[0].description;
             var list = template.list(topics);
             var html = template.HTML(title, list,
-              `<h2>${title}</h2>${description}`,
+              `<h2>${title}</h2>${description}
+               <p>by ${topic[0].name}</p>
+              `
+              ,
               ` <a href="/create">create</a>
                 <a href="/update?id=${queryData.id}">update</a>
                 <form action="delete_process" method="post">
